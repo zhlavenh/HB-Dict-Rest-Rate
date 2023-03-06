bab27922f2b9356929dc350ec12d3a434381550d
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-06 16:37:56 -0500

    Completed ratings funct

--- ratings.py
+++ ratings.py
@@ -1,4 +1,18 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+def resturant_ratings(filename):
+    with open(filename, "r") as reader:
+        ratings = {}
+
+        for line in reader:
+            line = line.rstrip().split(":")
+            ratings[line[0]] = line[1]
+
+        sorted_rating = sorted(ratings)
+
+        for show in sorted_rating:
+            print (f"{show} is rated a {ratings[show]}. ")
+
+        
+resturant_ratings("scores.txt")
