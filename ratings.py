bdc76acab67a50248b057ad9887a5ea8caef769b
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-06 16:55:52 -0500

    Added user input option

--- ratings.py
+++ ratings.py
@@ -2,17 +2,31 @@
 
 
 def resturant_ratings(filename):
+    ratings = {}
+
+# Reading file and storing it in dictionary
     with open(filename, "r") as reader:
-        ratings = {}
 
         for line in reader:
             line = line.rstrip().split(":")
             ratings[line[0]] = line[1]
 
-        sorted_rating = sorted(ratings)
+#Asking user for prompt and adding input to dictionary
+    add_resturant = input("Info from file retrieved.\nDo you want to add a resturant. Y/N: ")
 
-        for show in sorted_rating:
-            print (f"{show} is rated a {ratings[show]}. ")
+    while add_resturant == "Y" or add_resturant == "y":
+        res_name = input("Enter resturant name: ")
+        res_score = input("Enter score: ")
+        ratings[res_name] = res_score
+        add_resturant = input("Do you want to add a resturant. Y/N: ")
+
+#Sorting dictionary
+    sorted_rating = sorted(ratings)
+
+#print dictionary 
+    for show in sorted_rating:
+        print (f"{show} is rated a {ratings[show]}. ")
+    
 
 
 if __name__ == '__main__':    
